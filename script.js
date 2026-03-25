f4985a6d60e4bd5c6ac74b0f0dc0913c6dd81f64
Atlanq093 <laura.lewtak@gmail.com>
2026-03-25 16:56:43 +0100

    choices options

---
+++ script.js
@@ -0,0 +1,28 @@
+const owl = document.querySelector('.owl img');
+const note = document.getElementById('note');
+const question = document.querySelector('.container p.hidden');
+const yesBtn = document.getElementById('yesBtn');
+const noBtn = document.getElementById('noBtn');
+
+
+
+owl.addEventListener('click',()=>{
+    note.classList.add('hidden');
+    question.classList.remove('hidden');
+    yesBtn.classList.remove('hidden');
+    noBtn.classList.remove('hidden')
+})
+
+
+function answeryes(){
+    question.textContent="See you soon 😍";
+    owl.src ="images/2.png"
+    yesBtn.classList.add('hidden');
+    noBtn.classList.add('hidden');
+}
+function answerNo(){
+    question.textContent="Nuh uh?";
+    owl.src ="images/3.png"
+    yesBtn.classList.add('hidden');
+    noBtn.classList.add('hidden');
+}

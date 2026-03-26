fa77aff70ffc9ae85d2501b80b331f215e914a76
Atlanq093 <laura.lewtak@gmail.com>
2026-03-26 23:16:45 +0100

    repair bugs

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ const question = document.querySelector('.container p.hidden');
 const yesBtn = document.getElementById('yesBtn');
 const noBtn = document.getElementById('noBtn');
 
-
+const messages = [
+    "Why? 🥺",
+    "Why are you doing this to me? 😢",
+    "No please, think again... 💔",
+    "Wait, wait no... 😨",
+    "Please agree 🤕",
+    "I'm begging you... 🙏",
+    "You're breaking my heart 💔",
+    "Are you sure? 😳",
+    "I will cry... 😭",
+    "This is not okay 😤",
+    "I thought you liked me... 🥀",
+    "One more chance? 🫶",
+    "I made you a playlist and everything... 🎵",
+    "My owl is also sad now 🦉💔",
+    "Last chance I promise... 🥹",
+    "Click yes, please 😰"
+];
 
 function owlClick() {
     note.classList.add('hidden');
@@ -15,72 +32,35 @@ function owlClick() {
 
 owl.addEventListener('click', owlClick);
 
-
 function answeryes(){
     question.textContent="I knew it see you soon😍";
     owl.src ="images/2.png"
     yesBtn.classList.add('hidden');
     noBtn.classList.add('hidden');
-   owl.removeEventListener('click', owlClick); 
-  setTimeout(() => {
-    document.body.style.backgroundImage = "url('https://img1.picmix.com/output/stamp/normal/1/8/4/9/1329481_6cf09.gif')";
-    document.body.style.backgroundSize = "cover";
-    document.body.style.backgroundPosition = "center";
-}, 500);
+    owl.removeEventListener('click', owlClick); 
+    setTimeout(() => {
+        document.body.style.backgroundImage = "url('https://img1.picmix.com/output/stamp/normal/1/8/4/9/1329481_6cf09.gif')";
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundPosition = "center";
+    }, 500);
 }
-let clickCount=0
-let yesScale = 1;
-function answerNo(){
-   
-    owl.src ="images/3.png"
-    clickCount++
 
-    if(clickCount===1){
-        question.textContent="Why? 🥺";
+let clickCount = 0;
+let yesScale = 1;
+
+function answerNo(){
+    owl.src = "images/3.png";
+
+    if (clickCount === 0) {
         document.body.style.backgroundImage = "url('https://media.giphy.com/media/3o6ozmF4ZGOG3DWZyg/giphy.gif')";
-         document.body.style.backgroundSize = "cover";
-    document.body.style.backgroundPosition = "center";
-    } else if (clickCount === 2) {
-        question.textContent = "Why are you doing this to me? 😢";
-    } else if (clickCount === 3) {
-        question.textContent = "No please, think again... 💔";
-    } else if (clickCount === 4) {
-        question.textContent = "Wait,wait no ... 😨";
-     } else if (clickCount===5){
-            question.textContent="Please agree 🤕"
-        }
-        else if (clickCount===6){
-            question.textContent="Please agree 🤕"
-        }
-        else if (clickCount===8){
-             question.textContent = "I'm begging you... 🙏";
-        }
-        else if (clickCount===9){
-               question.textContent = "You're breaking my heart 💔";
-        }
-         else if (clickCount === 10) {
-        question.textContent = "Are you sure? 😳";
-    } else if (clickCount === 11) {
-        question.textContent = "I will cry... 😭";
-    } else if (clickCount === 12) {
-        question.textContent = "This is not okay 😤";
-    } else if (clickCount === 13) {
-        question.textContent = "I thought you liked me... 🥀";
-    } else if (clickCount === 14) {
-        question.textContent = "One more chance? 🫶";
-    } else if (clickCount === 15) {
-        question.textContent = "I made you a playlist and everything... 🎵";
-    } else if (clickCount === 16) {
-        question.textContent = "My owl is also sad now 🦉💔";
-    } else if (clickCount === 17) {
-        question.textContent = "Last chance I promise... 🥹";}
-    else{
-        question.textContent ="Click yes ,please 😰"
-       
-      
-} yesScale += 0.1; 
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundPosition = "center";
+    }
+
+    question.textContent = messages[Math.min(clickCount, messages.length - 1)];
+    clickCount++;
+
+    yesScale += 0.1;
     yesBtn.style.transform = `scale(${yesScale})`;
     yesBtn.style.transformOrigin = 'center';
-   
-  
 }
